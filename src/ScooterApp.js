9258340c5ee93e660ff8a6dd91a86a068dc849dc
sunnidaze7 <pnykii98@gmail.com>
2023-06-24 01:15:10 -0500

    finished scooterapp class and tests/ all tests and classes complete

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -3,35 +3,73 @@ const Scooter = require('./Scooter')
 
 class ScooterApp{
   // ScooterApp code here
-  constructor(stations, registeredUsers){
-    this.stations = stations
-    this.registeredUsers = registeredUsers
+  constructor(){
+    this.registeredUsers = {}
+    this.stations = {'station1':[], 'station2':[], 'station3':[]}
   }
-  registeredUser(){
-    
+  registeredUser(username,password,age){
+      if(age<18){
+        throw new Error('too young to register')
+      }else if(this.registeredUsers.hasOwnProperty(username)){
+        throw new Error('already registered')
+      }
+      else if(!this.registeredUsers.hasOwnProperty(username) && age
+          >= 18){
+            this.registeredUsers[username] = new User(username,password,age,false)
+            console.log('this user has been registered')
+          }
+        return this.registeredUsers
   }
-  // registeredUser(username,password,age){
-  //   for(let i = 0; i < this.registeredUsers.length;i++){
-  //     if(!this.registeredUsers[i].includes(username) && age
-  //     >= 18){
-  //       this.registeredUsers[i].username = username
-  //       this.registeredUsers[i].password = password
-  //       this.registeredUsers[i].age = age
-  //       console.log('this user has been registered')
-  //     }else if(age < 18){
-  //       console.log('too young to register')
-  //     }else{
-  //       console.log('already registered')
-  //     }
-  //   }
-  // }
-  // loginUser(username,password){
-  //   for(let i= 0;i <this.registeredUsers.length;i++){
-  //     if(this.registeredUsers[i].username == username && this.registeredUsers[i].password == password){
-        
-  //     }
-  //   }
-  // }
+  loginUser(username,password){
+    if(!this.registeredUsers.hasOwnProperty(username) || this.registeredUsers[username].password !== password){
+      throw new Error('username or password is incorrect')
+    }else{
+      this.registeredUsers[username].loggedIn = true
+      console.log('user has been logged in')
+    }
+    return this.registeredUsers[username].loggedIn
+  }
+  logoutUser(username){
+    if(!this.registeredUsers.hasOwnProperty(username)){
+      throw new Error('no such user is logged in')
+    }else{
+      this.registeredUsers[username].loggedIn = false
+      console.log('user has been logged out')
+    }
+    return this.registeredUsers[username].loggedIn
+  }
+  createScooter(station){
+    if(!this.stations.hasOwnProperty(station)){
+      throw new Error('no such station')
+    }
+    this.stations[station].push(new Scooter(station))
+    console.log('created new scooter')
+    return this.stations
+  }
+  dockScooter(scooter, station){
+    if(!this.stations.hasOwnProperty(station)){
+      throw new Error('no such station')
+    }else if(this.stations[station].includes(scooter)){
+      throw new Error('scooter already at station')
+    }else{
+      console.log('scooter is docked')
+      this.stations[station].push(scooter)
+    }
+    return this.stations
+  }
+  rentScooter(scooter,user){
+    if(!this.stations.hasOwnProperty(scooter.station)){
+      throw new Error('scooter already rented')
+    }else{
+      const index = this.stations[scooter.station].indexOf(scooter)
+      this.stations[scooter.station].splice(index,1)
+    }
+   return this.stations
+}
+  print(){
+    console.log(this.registeredUsers, '============')
+    console.log(this.stations, '===========')
+  }
 }
 
 module.exports = ScooterApp
--- tests/ScooterApp.test.js
+++ tests/ScooterApp.test.js
@@ -4,26 +4,83 @@ const ScooterApp = require('../src/ScooterApp')
 
 // ScooterApp tests here
 describe('scooterapp tests', ()=>{
-    const alice = new User('Alice', 'password1', 18, true)
+    const alice = new User('Alice', 'password1', 17, true)
     const scooter1 = new Scooter('station1')
-    const frank = new User('Frank', 'password2', 18, true)
     const scooter2 = new Scooter('station2')
-    const scooter3 = new Scooter('station3')
-    const scooterApp1 = new ScooterApp({station:[scooter1,scooter2],station:[scooter3]}, {username: alice, username: frank})
+    const scooter3 = new Scooter('station4')
+    const frank = new User('Frank', 'password2', 18, false)
+    const sam = new User('Sam', 'password3', 18, false)
+    const scooterApp1 = new ScooterApp()
+    scooterApp1.registeredUsers = {'Sam':sam}
+    scooterApp1.stations['station1'] = [scooter1]
+
     test('stations is object with station location and docked scooters at that location',()=>{
-        expect(scooterApp1.stations()).toBe({station:[scooter1,scooter2],station:[scooter3]})
+        expect(scooterApp1.stations).toStrictEqual({'station1':[scooter1],'station2':[],'station3':[]})
     })
 
-// register user
-test('registereduser is object with username as key and user as value',()=>{
-    expect(scooterApp1.registeredUser()).toBe({username: alice, username: frank})
-})
+//register user
+    test('throws error if underage',()=>{
+        expect(()=>{
+            scooterApp1.registeredUser(alice.username,alice.password,alice.age)
+        }).toThrow('too young to register')
+    })
+    test('registers user', ()=>{
+        expect(scooterApp1.registeredUser(frank.username,frank.password,frank.age)).toStrictEqual({'Frank':frank,'Sam':sam})
+    })
+    test('throws error if user already exists',()=>{
+        expect(()=>{
+            scooterApp1.registeredUser(sam.username,sam.password,sam.age)
+         }).toThrow('already registered')
+    })
 // log in
-
+    test('throws error if username or password do not match',()=>{
+        expect(()=>{
+            scooterApp1.loginUser(alice.username, 'password2')
+        }).toThrow('username or password is incorrect')
+    })
+    test('logs in user if username and password match',()=>{
+        expect(scooterApp1.loginUser(sam.username, sam.password)).toBe(true)
+    })
 // log out
-
-// rent scooter
-
+    test('throws error if user not logged in',()=>{
+        expect(()=>{
+            scooterApp1.logoutUser(alice.username, 'password2')
+        }).toThrow('no such user is logged in')
+    })
+    test('logs out user if username match',()=>{
+        expect(scooterApp1.logoutUser(sam.username, sam.password)).toBe(false)
+    })
+// create scooter
+    test('throws error if station does no exist', ()=>{
+        expect(()=>{
+            scooterApp1.createScooter('station4')
+        }).toThrow('no such station')
+    })
+    test('creates new scooter and logs in station', ()=>{
+        expect(scooterApp1.createScooter('station1')).toBe(scooterApp1.stations)
+    })
 // dock scooter
-
+    test('throws error if station does not exist',()=>{
+        expect(()=>{
+            scooterApp1.dockScooter(scooter1, 'station4')
+        }).toThrow('no such station')
+    })
+    test('throws error if scooter already at station',()=>{
+        expect(()=>{
+            scooterApp1.dockScooter(scooter1, 'station1')
+        }).toThrow('scooter already at station')
+    })
+    test('docks scooter at station',()=>{
+        expect(scooterApp1.dockScooter(scooter2,'station2')).toBe(scooterApp1.stations)
+    })
+//rent scooter
+    test('throws error if scooter already rented', ()=>{
+        expect(()=>{
+            scooterApp1.rentScooter(scooter3,alice)
+        }).toThrow('scooter already rented')
+    })
+    test('rent scooter to user',()=>{
+        expect(scooterApp1.rentScooter(scooter1,sam)).toBe(scooterApp1.stations)
+    })
 })
+
